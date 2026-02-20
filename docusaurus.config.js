ea697a7df8e3b3e18dc3a7e18bec22db8e71e52d
c4vxl <manage@c4vxl.de>
2026-02-20 22:39:48 +0100

    Updated url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
 
-  url: 'https://gamemanagementapi.github.io',
+  url: 'https://gma.c4vxl.de/',
   baseUrl: '/docs/',
   projectName: 'docs',
   organizationName: 'GameManagementAPI',
